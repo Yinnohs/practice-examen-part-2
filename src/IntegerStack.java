0a293083b4a07407b08c77bba6784331b07b0880
josesoto <josesoto@itop.es>
2022-05-05 10:08:43 +0100

    fat: stack created

---
+++ src/IntegerStack.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class IntegerStack {
+    private ArrayList<Integer> values;
+
+    public IntegerStack (){
+        this.values = new ArrayList<>();
+    }
+
+    public void push (Integer element){
+        this.values.add(element);
+    }
+
+    public  Integer pop (){
+        this.values.remove(this.values.size()-1);
+    }
+}
